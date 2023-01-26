5829f18ab39336a50a4824c54ed31564f1610269
조혜연 <hy_312@mncnext.co.kr>
2023-01-26 13:42:06 +0900

    Chore: tailwindcss 설치

--- src/Pages/Intro/index.tsx
+++ src/Pages/Intro/index.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 
-type Props = {
-  title?: string;
+const Intro: React.FC = () => {
+  return <div className="text-orange-500">Intro Page</div>;
 };
 
-const Intro: React.FC<React.PropsWithChildren<Props>> = ({ children }) => <div>헬로우</div>;
 export default Intro;
